eddcb32d44a4ffc2e4bba5d030f1d7f53eb0ed4f
Steve Hayes <steven.hayes@gmail.com>
2024-10-18 15:41:22 +0100

    prep for connect

---
+++ test-service/src/app.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send({ message: "root GET responded..." });
+});
+
+app.post("/", (req: Request, res: Response) => {
+  console.dir(req.body);
+  res.status(200).send({ message: "root POST responded..." });
+});
+
+export default app;
