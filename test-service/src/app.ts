6cb1acd4582b462d4db681878e9ceb7b94fd8ff8
Steve Hayes <steven.hayes@gmail.com>
2024-10-18 15:57:56 +0100

    test service post endpoint

--- test-service/src/app.ts
+++ test-service/src/app.ts
@@ -14,4 +14,9 @@ app.post("/", (req: Request, res: Response) => {
   res.status(200).send({ message: "root POST responded..." });
 });
 
+app.post("/test-service", (req: Request, res: Response) => {
+  console.dir(req.body);
+  res.status(200).send({ message: "test-service POST responded..." });
+});
+
 export default app;
