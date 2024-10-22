e161a149ae4f048e6e1d802047a351ae0f09122e
Steve Hayes <steven.hayes@gmail.com>
2024-10-22 13:59:59 +0100

    cart to IE

--- test-service/src/controllers/cart.controller.ts
+++ test-service/src/controllers/cart.controller.ts
@@ -74,7 +74,7 @@ const update = async (resource: Resource) => {
 
     const setCountryAction: UpdateAction = {
       action: 'setCountry',
-      country: 'US',
+      country: 'IE',
     };
 
     // Create the UpdateActions Object to return it to the client
